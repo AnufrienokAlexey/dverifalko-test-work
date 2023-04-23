24d5e9aec87e5a2f69b3f07fd9a120d9f4260eae
Aleksey Anufrienok <109474129+AnufrienokAlexey@users.noreply.github.com>
2023-04-23 03:20:59 +0300

    Delete telegramBot.php

--- app/lib/telegramBot.php
+++
@@ -1,8 +0,0 @@
-<?php
-$token = '6246619275:AAFhpIHjVq9w82Mz8j6pEaPEyZPq5dHnwIo';
-$name = 'dverifalko';
-$nick = 'dverifalko_bot';
-$chatId = '-919708435';
-
-//{"ok":true,"result":[{"update_id":430801898,
-//"message":{"message_id":7,"from":{"id":727047490,"is_bot":false,"first_name":"\u0410\u043b\u0435\u043a\u0441\u0435\u0439","username":"alexeyanufrienok","language_code":"ru"},"chat":{"id":-919708435,"title":"\u0417\u0430\u044f\u0432\u043a\u0438 \u0441 \u0441\u0430\u0439\u0442\u0430 Dverifalko","type":"group","all_members_are_administrators":true},"date":1682118154,"text":"/join @dverifalko_bot","entities":[{"offset":0,"length":5,"type":"bot_command"},{"offset":6,"length":15,"type":"mention"}]}}]}
